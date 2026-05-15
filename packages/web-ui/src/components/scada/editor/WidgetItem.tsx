3554d18f9b6322af4caf787a61bcef6c8310989c
kris <wangkris817@gmail.com>
2026-05-15 07:54:25 -0700

    feat(web-ui): scada editor EditorCanvas (drop zone, WidgetItem map, deselect) + 3 tests (stub WidgetItem)

---
+++ packages/web-ui/src/components/scada/editor/WidgetItem.tsx
@@ -0,0 +1,4 @@
+'use client';
+import React from 'react';
+import type { WidgetDef } from '@/widgets';
+export function WidgetItem(_props: { widget: WidgetDef; isSelected: boolean; onSelect: (id: string) => void; onMove: (id: string, x: number, y: number) => void; onResize: (id: string, w: number, h: number) => void }) { return null; }
